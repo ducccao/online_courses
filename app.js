35668ac989be2d3d446d5e0e8c2b0b191bdd0a9f
Duc Cao <caovanducs@gmail.com>
2020-11-13 16:43:44 +0700

    Setup Folder

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const expressHandleBars = require("express-handlebars");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.end("Hi world!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server is start at ", PORT);
+});
