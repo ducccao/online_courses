fc97da9ba362b27715e0867bfe9c67971429081a
Duc Cao <caovanducs@gmail.com>
2020-12-14 16:25:42 +0700

    Helper compare handlebars

--- utils/categoryDatabase.js
+++ utils/categoryDatabase.js
@@ -1,17 +1,17 @@
 const randomString = require("./getRandomString");
 
 const fakeCateDB = [
-  { cateID: 1, cateName: "ReactJS", keyLevel: 1 },
-  { cateID: 2, cateName: "Javascript", keyLevel: 1 },
-  { cateID: 3, cateName: "Flutter", keyLevel: 0 },
-  { cateID: 4, cateName: "React Native", keyLevel: 0 },
+  // { cateID: 1, cateName: "ReactJS", keyLevel: 1 },
+  // { cateID: 2, cateName: "Javascript", keyLevel: 1 },
+  // { cateID: 3, cateName: "Flutter", keyLevel: 0 },
+  // { cateID: 4, cateName: "React Native", keyLevel: 0 },
 ];
 
 console.log(randomString(5));
 
-for (let i = 5; i < 30; i++) {
+for (let i = 0; i < 16; i++) {
   fakeCateDB.push({
-    PcateID: 1,
+    cateID: i,
     cateName: randomString(5),
     keyLevel: Math.random() >= 0.5 ? 1 : 0,
   });
