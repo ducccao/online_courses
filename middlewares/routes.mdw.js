4958eae6791bcfbed45e4f92231438453bcf9e59
Duc Cao <caovanducs@gmail.com>
2020-12-28 22:50:15 +0700

    Fix conflict

--- middlewares/routes.mdw.js
+++ middlewares/routes.mdw.js
@@ -1,37 +1,21 @@
-<<<<<<< HEAD
-module.exports = function(app) {
-    // routers
-    app.use(require("./../routers/main.router"));
-    app.use("/admin", require("./../routers/admin.router"));
-    app.use("/user", require("./../routers/user.router"));
-    app.use("/account", require("./../routers/account.router"));
-=======
 module.exports = function (app) {
   // routers
   app.use(require("./../routers/main.router"));
   app.use("/admin", require("./../routers/admin.router"));
   app.use("/admin", require("./../routers/course.router"));
   app.use("/user", require("./../routers/user.router"));
->>>>>>> phase2/server
 
-    // app.use(express.static("client"));
+  // app.use(express.static("client"));
 
-<<<<<<< HEAD
-    // main router
-    app.get("/", (req, res) => {
-        console.log(req.session.isAuth);
-        console.log(req.session.authUser);
-=======
   // main router
   app.get("/", (req, res) => {
     // console.log(req.session.isAuth);
     // console.log(req.session.authUser);
->>>>>>> phase2/server
 
-        res.render("vwMain/Home");
-    });
+    res.render("vwMain/Home");
+  });
 
-    app.get("/about", (req, res) => {
-        res.render("vwMain/About");
-    });
+  app.get("/about", (req, res) => {
+    res.render("vwMain/About");
+  });
 };
--- utils/db.js
+++ utils/db.js
@@ -3,42 +3,18 @@ const { off } = require("process");
 const util = require("util");
 
 var pool = mysql.createPool({
-    host: "localhost",
-    port: "3306",
-    user: "root",
-    password: "long123",
-    database: "onlinecourses",
-    connectionLimit: 50,
+  host: "localhost",
+  port: "3306",
+  user: "duccao",
+  password: "duc123",
+  database: "onlinecourses",
+  connectionLimit: 50,
 });
 
 // promisify bind pool to a promise and remove callback
 const poo_query = util.promisify(pool.query).bind(pool);
 
 module.exports = {
-<<<<<<< HEAD
-    load: (sql) => {
-        return poo_query(sql);
-    },
-    add: (entity, tableName) => {
-        return poo_query(`insert into ${tableName} set ? `, entity);
-    },
-    insertStuffIntoTable: (entity, tblName) => {
-        return poo_query(`insert into ${tblName} set ? `, entity);
-    },
-    patch: (entity, condition, tblName) => {
-        console.log("ham patch ne");
-        return poo_query(`update ${tblName} set ? where ?`, [entity, condition]);
-    },
-    del: (condition, tblName) => {
-        const sql = `delete from ${tblName} where ?`;
-        return poo_query(sql, condition);
-    },
-    getCateByID: (catID) => {
-        const sql = `select * from category where catID = ${catID}`;
-        return poo_query(sql);
-    },
-};
-=======
   load: (sql) => {
     return poo_query(sql);
   },
@@ -58,4 +34,3 @@ module.exports = {
     return poo_query(sql);
   },
 };
->>>>>>> phase2/server
