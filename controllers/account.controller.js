97f0a11ef1cf292a034f49a7b344009d57914df2
Duc Cao <caovanducs@gmail.com>
2021-01-09 11:48:14 +0700

    Fix Profile Frontend, Update Change Password

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -1,27 +1,25 @@
 const moment = require("moment");
-const accountModel = require('../models/account.model');
+const accountModel = require("../models/account.model");
 const bcryptjs = require("bcryptjs");
 module.exports = {
     getProfile: (req, res) => {
         console.log("get Profile");
         if (req.session.isAuth === true) {
-
             res.locals.authUser.DOB = moment().format("MM/DD/YYYY");
             res.render("vwAccount/profile", {
                 layout: "account",
-            })
+            });
         } else {
-
             throw Error("access denied");
         }
     },
     getChangePass: (req, res) => {
-        console.log("get change pass")
+        console.log("get change pass");
         if (req.session.isAuth === true) {
             res.render("vwAccount/changePass", {
                 layout: "account",
-                session: req.session
-            })
+                session: req.session,
+            });
         } else {
             throw Error("access denied");
         }
@@ -29,19 +27,18 @@ module.exports = {
     getPhoto: (req, res) => {
         if (req.session.isAuth === true) {
             res.render("vwAccount/photo", {
-                layout: "account"
-            })
+                layout: "account",
+            });
         } else {
             throw Error("access denied");
         }
-
     },
     postProfile: async(req, res) => {
         if (req.session.isAuth === true) {
             try {
                 console.log("updating profile!");
-                const { username, email, dob } = req.body
-                    //  console.log(req.body);
+                const { username, email, dob } = req.body;
+                //  console.log(req.body);
 
                 // format date - yyyy-mth-day
                 const dobFormated = moment(dob, "DD/MM/YYYY").format("YYYY-MM-DD");
@@ -52,15 +49,14 @@ module.exports = {
                 };
                 console.log(user);
 
-                const condition = { email: email }
+                const condition = { email: email };
 
-                const result = await accountModel.updateProfile(user, condition)
+                const result = await accountModel.updateProfile(user, condition);
 
                 // req.session.authUser.dob = DOB;
                 req.session.authUser.userName = user.userName;
 
                 res.end();
-
             } catch (er) {
                 // console.log(er);
                 return res.status(404).json({ message: er.sqlMessage });
@@ -68,53 +64,75 @@ module.exports = {
         } else {
             throw Error("access denied");
         }
-
     },
     getLogOut: async(req, res) => {
         req.session.isAuth = false;
         req.session.authUser = null;
 
-
-        res.redirect('../user/login');
+        res.redirect("../user/login");
     },
 
     postChangePass: async(req, res) => {
         if (req.session.isAuth === true) {
-            try {
-                console.log("updating password!");
-                const { oldPass, newPass } = req.body
+            console.log(req.session.authUser);
+
+            if (req.session.authUser.decentralization === 2) {
+                const { oldPass, newPass } = req.body;
+                if (oldPass === "admin123") {
+                    const condition = { email: req.session.authUser.email };
+                    //  console.log(condition);
+                    const user = {
+                        password: newPass,
+                    };
+                    const result = await accountModel.updatePassword(user, condition);
+                    res.status(200).json({ message: "Updated Password!" });
+                } else {
+                    return res
+                        .status(404)
+                        .json({ message: "Invalid email or password!" });
+                }
+            } else {
+                try {
+                    console.log("updating password!");
+                    const { oldPass, newPass } = req.body;
                     //  console.log(req.body);
 
-                const hash = bcryptjs.hashSync(newPass, 10);
-                const user = {
-                    password: hash
-                };
+                    const hash = bcryptjs.hashSync(newPass, 10);
+                    const user = {
+                        password: hash,
+                    };
 
-                const comparePassword = bcryptjs.compareSync(
-                    oldPass,
-                    req.session.authUser.password
-                );
-                console.log(comparePassword);
-                if (comparePassword === false) {
-                    return res.status(404).json({ message: "Invalid email or password!" });
-                } else {
-                    const condition = { email: req.session.authUser.email }
-                    console.log(condition);
-                    const result = await accountModel.updatePassword(user, condition);
+                    const comparePassword = bcryptjs.compareSync(
+                        oldPass,
+                        req.session.authUser.password
+                    );
+                    console.log(comparePassword);
+                    if (comparePassword === false) {
+                        return res
+                            .status(404)
+                            .json({ message: "Invalid email or password!" });
+                    } else {
+                        const condition = { email: req.session.authUser.email };
+                        console.log(condition);
+                        const result = await accountModel.updatePassword(user, condition);
 
-                    req.session.authUser.password = hash
+                        req.session.authUser.password = hash;
+                    }
+
+                    res.end();
+                } catch (er) {
+                    // console.log(er);
+                    return res.status(404).json({ message: er.sqlMessage });
                 }
-
-
-                res.end();
-
-            } catch (er) {
-                // console.log(er);
-                return res.status(404).json({ message: er.sqlMessage });
             }
         } else {
             throw Error("access denied");
         }
-
     },
-}
+
+    getPurchasedCourses: (req, res) => {
+        res.render("vwAccount/PurchasedCourses", {
+            layout: "main",
+        });
+    },
+};
